fab31f98b508bbcbd8417e221921bf1968f8dfe5
Michelle Soderstrand <misode@hotmail.com>
2025-12-05 14:53:02 +0100

    fixed navmobile. have a great weekend

--- the-idea-adventure/app/components/nav.tsx
+++ the-idea-adventure/app/components/nav.tsx
@@ -1,28 +1,55 @@
 import { NavLink } from "react-router-dom";
-import React from "react";
-
+import React, { useState } from "react";
 
 const Nav: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   const getActiveClass = ({ isActive }: { isActive: boolean }) =>
     isActive ? "nav-link active" : "nav-link";
 
   return (
     <nav className="nav-container">
-      <NavLink to="/" ><h2 className="logo">LOGO</h2></NavLink>
+      <NavLink to="/">
+        <h2 className="logo">LOGO</h2>
+      </NavLink>
 
-      <ul className="nav-menu">
+      {/* Hamburger Icon */}
+      <div
+        className={`hamburger ${isOpen ? "open" : ""}`}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+      {/* Menu */}
+      <ul className={`nav-menu ${isOpen ? "show" : ""}`}>
         <li>
-          <NavLink to="/" className={getActiveClass} end>
+          <NavLink
+            to="/"
+            className={getActiveClass}
+            end
+            onClick={() => setIsOpen(false)}
+          >
             Home
           </NavLink>
         </li>
         <li>
-          <NavLink to="/project-info" className={getActiveClass}>
+          <NavLink
+            to="/project-info"
+            className={getActiveClass}
+            onClick={() => setIsOpen(false)}
+          >
             Info
           </NavLink>
         </li>
         <li>
-          <NavLink to="/navplaygame" className={getActiveClass}>
+          <NavLink
+            to="/navplaygame"
+            className={getActiveClass}
+            onClick={() => setIsOpen(false)}
+          >
             Play Game
           </NavLink>
         </li>
@@ -32,4 +59,3 @@ const Nav: React.FC = () => {
 };
 
 export default Nav;
-
